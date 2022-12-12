edfa0dee1f674b09eec8824a1a69da2479df5b5c
mizanor21 <mizanorrahman398@gmail.com>
2022-12-12 20:44:08 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='flex'>
+            <div className="w-1/2 border-2 p-28">
+
+            </div>
+            <div className="w-1/2 border-2">
+                <video autoPlay loop className='w-[500px]' src="https://cdnl.iconscout.com/lottie/premium/preview-watermark/web-development-4179004-3479083.mp4"></video>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/layouts/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+import Navbar from '../components/Navbar/Navbar'
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Header></Header>
+        </div>
+    );
+};
+
+export default Main;
